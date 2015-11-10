d7bd4f4784a5564ea0d06c494664e0c1a6740163
yenrab <barney.cit@gmail.com>
2015-11-10 12:46:20 -0700

    updated the license in each source code file to MIT

--- SwiftlyFunc/SwiftlyFunc/SwiftlyFunc.swift
+++ SwiftlyFunc/SwiftlyFunc/SwiftlyFunc.swift
@@ -1,29 +1,28 @@
-//
-//  AppInit.swift
-//  SwiftlyFunc
-//
-//  Created by Lee Barney on 10/27/15.
-//  Copyright © 2015 Lee Barney. All rights reserved.
-//
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Lee Barney
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 import UIKit
-
-/*
-* The purpose of SwiftlyFunc is to allow developers to begin coding their
-* app functionally without having to deal with so much OO. MVC is an OO
-* pattern and doesn't necissarily fit as a functional design pattern. It
-* could, if rethought.
-*
-* To do's:
-* 1) hide AppDelegate and ViewController.
-* 2) create functions that map closures to app eve
-@IBOutlet weak var theButton: UIButton!
-nts
-* 3) abandon the ViewController class. Map UI elements to closures. Create
-* a single IBAction for for all touchable UIElements. Back it with an 'app
-* controller' like functionality mapping the element's identifier with
-* a closure.
-*/
 
 enum AppEventType:String{
     case appWillResignActive
